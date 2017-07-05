6da379d90c1507501abdba78571a4ee117791fd1
Salvador Guzman <guzmansalv@gmail.com>
2017-07-04 23:36:29 -0700

    moved functions to words.js

--- js/words.js
+++ js/words.js
@@ -173133,3 +173133,24 @@ words.forEach(function(wd) {
     wordListElementsHash.set(wd, w);
     document.getElementById('list-content').appendChild(w);
 });
+
+function wordRegexFilter(regex) {
+    return words.filter(s => s.match(regex));
+}
+
+function createListFromWords(wordList) {
+    wordListElementsHash.forEach(function(key) {
+        key.hidden = true;
+    });
+    wordList.forEach(function(w) {
+        wordListElementsHash.get(w).hidden = false;
+    });
+}
+
+function onClick() {
+    const ele = document.getElementById('text-box');
+    const reg = new RegExp(ele.value);
+    createListFromWords(wordRegexFilter(reg));
+}
+
+document.getElementById('button').onclick = onClick;
